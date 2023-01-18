8040ef5ea0ff1821b1c2c4120c9e41e2210d5748
Luckas <luckasranarison@gmail.com>
2023-01-18 09:35:31 +0300

    add: parseInfo

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -42,4 +42,48 @@ const rubyParser = (str: string): string => {
     return str;
 };
 
-export { parseRomajiLyrics, parseKanjiLyrics };
+const infoMap: any = {
+    Episodes: "episodes",
+    Description: "description",
+    "Japanese Title": "japanseTitle",
+    "English Title": "englishTitle",
+    "From Anime": "anime",
+    "From Season": "season",
+    "Performed by": "artist",
+    "Lyrics by": "lyricsWritter",
+    "Composed by": "compositor",
+    "Arranged by": "arrangement",
+    "Released:": "releaseDate",
+};
+
+const parseInfo = (info: string): any => {
+    const songInfo: any = {};
+
+    if (info) {
+        const match = info.match(
+            /<strong>(.*?)<\/strong>(?::|[\s\r\n]+)(.*?)<br>/gs
+        );
+
+        if (match) {
+            match.forEach((value) => {
+                const singleMatch = value.match(
+                    /<strong>(.*?)<\/strong>(?::|[\s\r\n]+)(.*?)<br>/s
+                );
+                if (singleMatch) {
+                    const key = singleMatch[1].trim().replace("\n", "");
+                    let prop = singleMatch[2].trim().replace("\n", "");
+                    const infoKey = infoMap[key];
+                    const matchLink = prop.match(/<a.*?>(.*?)<\/a>/s); // nested tag
+
+                    if (matchLink) prop = matchLink[1];
+
+                    songInfo[infoKey] = prop;
+                }
+            });
+        }
+    }
+
+    return songInfo;
+};
+
+export { parseRomajiLyrics, parseKanjiLyrics, parseInfo, infoMap };
